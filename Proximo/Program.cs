bc8a179d4552b3dc7972b195af0593227e2c37e3
Rahul Gupta <rahulgupta@Rahuls-MacBook-Pro.local>
2023-01-20 00:28:59 -0800

    intiatl commint

--- Proximo/Program.cs
+++ Proximo/Program.cs
@@ -7,6 +7,7 @@ namespace Proximo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("hell this is");
         }
     }
 }
