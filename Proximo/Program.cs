320ccb39fa69a83b4d9bd0e3b58a2782cff8e2a9
Rahul Gupta <rahulgupta@Rahuls-MacBook-Pro.local>
2023-01-20 02:36:08 -0800

    added new code for bobrental and test project

--- Proximo/Program.cs
+++ Proximo/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace Proximo
 {
@@ -10,8 +11,14 @@ namespace Proximo
         {
             var input = new List<string>() { "zopp","aab", "aabc", "rshaym", "sss" , "ss"};
             var result = SortList.CustomSort(input);
-           // result.ForEach(x => Console.WriteLine(x));
-            
+            // result.ForEach(x => Console.WriteLine(x));
+
+
+            BobRental bob = new BobRental();
+            var res = bob.TotalPrice("2023/01/1", 5);
+            Console.WriteLine(res);
+            Console.ReadKey();
+
         }
     }
 
@@ -34,6 +41,67 @@ namespace Proximo
 
             } 
             return  sortedList.OrderBy(x => x).ToList();
+
+        }
+    }
+
+    public class BobRental
+    {
+
+        private readonly Dictionary<int, int[]> dict = new Dictionary<int, int[]>();
+        
+        public BobRental()
+        {
+            dict.Add(1, new int[] { 50, 49, 48, 45 });
+
+            dict.Add(2, new int[] { 50, 49, 48, 45 });
+
+            dict.Add(3, new int[] { 50, 49, 48, 45 });
+
+            dict.Add(4, new int[] { 51, 50, 49, 48 }); 
+
+            dict.Add(5, new int[] { 54, 54, 54, 54 }); 
+        }
+
+        public string TotalPrice(string date, int days)
+        {
+            DateTimeOffset parsedDate;
+            if (!DateTimeOffset.TryParse(date, out parsedDate))
+            {
+                throw new ArgumentOutOfRangeException(nameof(date), "Invalid DateOffset");
+            }
+
+            var currentMonth = parsedDate.Month;
+            var currentDay = parsedDate.Day + days;
+
+            if (dict.ContainsKey(currentMonth))
+            {
+                var index = 0;
+
+                if (currentDay <= 3)
+                {
+                    index = 0;
+                }
+                else if (currentDay <= 8)
+                {
+                    index = 1;
+                }
+                else if (currentDay <= 15)
+                {
+                    index = 2;
+                }
+                else
+                {
+                    index = 3;
+                }
+                var result = dict[currentMonth][index] * days;
+                return result.ToString("C0");
+            }
+            else
+            {
+                throw new ArgumentException(nameof(currentMonth), "month not implemented");
+            }
+
         }
     }
 }
